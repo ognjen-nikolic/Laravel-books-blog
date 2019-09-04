aa92beddd14be6e516697121694851b1ae1c0eea
Ognjen <51864415+ognjen-nikolic@users.noreply.github.com>
2019-09-04 16:55:12 +0200

    Update autor.blade.php

--- pages/autor.blade.php
+++ pages/autor.blade.php
@@ -12,7 +12,7 @@
     </div>
     <div class="col-lg-6">
         <h2>Autor sajta</h2>
-        <h3>Miljana Šmigić 234/15</h3>
+        <h3>Ognjen Nikolic 29/15</h3>
         <p>Student sam završne godine Visoke ICT škole, smer Web programiranje i veliki sam ljubitelj knjiga.</p>
         <p>Sajt je napravljen u Laravel framework-u kao školski projekat za predmet Web programiranje PHP2.</p>
     </div>
